bfd31661b30a3fe9ed63dc8d6b404d6f31d24016
hongbin <ying.hongbin.1016@gmail.com>
2024-12-25 10:36:53 +0800

    fix: resolve CommitMessage.body parsed to unexpected rsult

--- src/commitMessage.ts
+++ src/commitMessage.ts
@@ -2,15 +2,15 @@ import * as core from '@actions/core';
 
 class CommitMessage {
   header: string;
-  body: string[] | null;
+  body: string[];
   hasBody: boolean = true;
   footer: string | null;
   hasFooter: boolean = true;
 
-  constructor(header: string, body: string[] | null, footer: string | null) {
+  constructor(header: string, body: string[], footer: string | null) {
     this.header = header;
     this.body = body;
-    if (body === null) {
+    if (body.length === 0) {
       this.hasBody = false;
     }
     this.footer = footer;
@@ -22,16 +22,18 @@ class CommitMessage {
 
 function parseMessage(message: string): CommitMessage {
   const sections: string[] = message.split('\n\n');
-  if (sections.length === 1) {
-    return new CommitMessage(sections[0], null, null);
+  const footerCandidate = sections[sections.length - 1];
+  if (
+    footerCandidate.startsWith('close') ||
+    footerCandidate.startsWith('fix')
+  ) {
+    return new CommitMessage(
+      sections[0],
+      sections.slice(1, sections.length - 1),
+      footerCandidate,
+    );
   } else {
-    const section = sections[sections.length - 1];
-    if (section.startsWith('close') || section.startsWith('fix')) {
-      const paragraph = sections.slice(1, sections.length - 1);
-      return new CommitMessage(sections[0], paragraph, section);
-    } else {
-      return new CommitMessage(sections[0], sections.slice(1), null);
-    }
+    return new CommitMessage(sections[0], sections.slice(1), null);
   }
 }
 
