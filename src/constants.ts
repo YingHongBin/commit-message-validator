b3224fecbb640ebd258fb127564dc9c5e1855969
hongbin <ying.hongbin.1016@gmail.com>
2024-12-30 13:29:51 +0800

    style: format code

--- src/constants.ts
+++ src/constants.ts
@@ -16,6 +16,7 @@ export const HEADER_PATTERN: RegExp = /^(.+?)(?:\((.+)\))?: (.+)$/;
 export const MERGE_PATTERN: RegExp = /^Merge pull request #[0-9]+ from .+$/;
 // footer must begin with keywords close or fix followed by a space and then #{issue_number}
 // multiple issues can be separated by commas like close #123, fix #456
-export const FOOTER_PATTERN: RegExp = /^((close|fix) #[0-9]+)(, (close|fix) #[0-9]+)*$/;
+export const FOOTER_PATTERN: RegExp =
+  /^((close|fix) #[0-9]+)(, (close|fix) #[0-9]+)*$/;
 // revert commit must be in the format: Revert "<commit_header>"
 export const REVERT_PATTERN: RegExp = /^Revert ".+"$/;
